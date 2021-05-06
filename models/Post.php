4a25da5c42edddc961f3ecf62100eaa4ef58e727
Mellow <cpryan@mail.fhsu.edu>
2021-05-05 19:27:10 -0500

    please

--- models/Post.php
+++ models/Post.php
@@ -22,11 +22,11 @@
         public function read() {
             // Create query
             $query = 'SELECT c.category as category_name, q.id, q.categoryID, q.quote, q.authorID, a.author as author_name FROM ' . $this->table . ' q LEFT JOIN categories c ON q.categoryID = c.id LEFT JOIN authors a ON q.authorID = a.id ORDER BY q.id';
-            if($this->authorID && $this->categoryID && $this->authorID > 0 && $this->categoryID > 0){
+            if($this->authorID && $this->categoryID){
                 $query = $query . ' WHERE q.authorId = :authorId AND q.categoryId = :categoryId';
-            } else if($this->authorID && $this->authorID > 0){
+            } else if($this->authorID){
                 $query = $query . ' WHERE q.authorId = :authorId';
-            } else if($this->categoryID && $this->categoryID > 0){
+            } else if($this->categoryID){
                 $query = $query . ' WHERE q.categoryId = :categoryId';
             }
             if($this->lim){
